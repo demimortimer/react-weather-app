196f5dd036ec4bd3c3799d0b48a57a81de8b83f1
demimortimer <153766264+demimortimer@users.noreply.github.com>
2024-05-15 19:57:00 +0100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <p>
         This project was coded by Demi Mortimer and is
         <a href="https://github.com/demimortimer"> open-sourced on Github</a>.
