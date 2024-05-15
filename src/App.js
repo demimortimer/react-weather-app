9c44b00dd9bcc42afabafe7553ddb295b1e5119f
demimortimer <153766264+demimortimer@users.noreply.github.com>
2024-05-15 19:38:04 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        This project was coded by Demi Mortimer and is
+        <a href="https://github.com/demimortimer"> open-sourced on Github</a>.
+      </p>
+    </div>
+  );
 }
